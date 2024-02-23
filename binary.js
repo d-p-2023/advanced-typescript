36748741a8dd17b74a7fd3b954984b7c0d5f97f9
Deva <deva.palanisamy@appsbroker.com>
2024-02-23 09:46:46 +0000

    added binary conversion

---
+++ binary.js
@@ -0,0 +1,5 @@
+var binaryConversion = function (num) {
+    return num.toString(2);
+};
+console.log(binaryConversion(10)); // Output: 1010
+console.log(binaryConversion(5)); // Output: 101
---
+++ binary.ts
@@ -0,0 +1,6 @@
+const binaryConversion = (num: number): string => {
+    return num.toString(2);
+}
+
+console.log(binaryConversion(10)); // Output: 1010
+console.log(binaryConversion(5)); // Output: 101
